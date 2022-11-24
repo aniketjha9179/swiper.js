b64bd6b196939f4c8613ef94a4086721d6406a89
aniketjha9179 <aniketjha9179@gmail.com>
2022-11-24 17:39:03 +0530

    swiper js

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
